d47efd6ca1eb10ec8f544cf877f544611a55f5e1
bien <maibien90@hotmail.com>
2021-01-17 19:28:05 +0700

    up

--- app/Services/WorkflowExport.php
+++ app/Services/WorkflowExport.php
@@ -2,6 +2,9 @@
 namespace App\Services;
 
 
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 class WorkflowExport {
 
     function __construct()
@@ -10,6 +13,16 @@ class WorkflowExport {
     }
 
     public function exportSample() {
+
+        $inputFileName =storage_path('app/templates/template_1.xlsx');
+
+        /** Load $inputFileName to a Spreadsheet Object  **/
+        $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
+//        $sheet = $spreadsheet->getActiveSheet();
+//        $sheet->setCellValue('A1', 'Hello World !');
+
+        $writer = new Xlsx($spreadsheet);
+        $writer->save(storage_path('app/public/helloworld.xlsx'));
         dd(2);
     }
 
