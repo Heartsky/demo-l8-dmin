4941894b35396a2985d9cc3b0910d0c1c09fbdac
bien <maibien90@hotmail.com>
2021-03-02 01:04:05 +0700

    update

--- database/migrations/2021_03_01_150037_create_companies_table.php
+++ database/migrations/2021_03_01_150037_create_companies_table.php
@@ -15,6 +15,7 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('code');
             $table->string('name');
             $table->string('address');
             $table->string('phone_number');
